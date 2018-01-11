505019970c472ab33e7d3d161899bed96ddc7dea
programoworm <rbkumar712@gmail.com>
2018-01-11 20:07:25 +0530

    Updated malloc

--- Pointer_is_array.c
+++ Pointer_is_array.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<malloc.h>
 int main()
 {
-	int i,a[5]={1,2,3,4,5},*b;
-	for(i=0;i<5;i++){
+	int i,a[5]={1,2,3,4,5},*b,n,*index;	
+	/*for(i=0;i<5;i++){
 		b=&a[i];
 		printf("%d\t%ld\n",a[i],b);
 	}
@@ -10,6 +11,18 @@ int main()
 	for(i=0;i<5;i++){
 	//	b=&a[i];
 		printf("%ld\t%d\n",b+i,*(b+i));
+	}*/
+	printf("Enter the number of elements in the array: ");
+	scanf("%d",&n);
+	index=(int*)malloc(n*sizeof(int));
+	b=index;
+	for(i=0;i<n;i++){
+		printf("Enter the element: ");
+		scanf("%d",b++);
+	}
+	b=index;
+	for(i=0;i<n;i++){
+		printf("%d\n",*(b+i));
 	}
 	return 0;
 }
